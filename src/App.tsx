5e5851e3bd4950bb5055e7373dc94843a69a1657
luizrosalba <luizrosalba@gmail.com>
2024-03-19 16:52:53 -0300

    Entra ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import './App.css'
 import { GoogleLogin } from '@react-oauth/google';
+import { MsalProvider, AuthenticatedTemplate, useMsal, UnauthenticatedTemplate } from '@azure/msal-react';
+import { IdTokenData } from './components/DataDisplay';
+import { loginRequest } from './authConfig';
 
-
-function App() {
-
+const MainContent = () => {
+  const { instance } = useMsal();
+  const activeAccount = instance.getActiveAccount();
+  const handleRedirect = () => {
+    instance
+      .loginRedirect({
+        ...loginRequest,
+        prompt: 'create',
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <>
       <h1>Example Authentication</h1>
-      <div>Google Test 123</div>
       <GoogleLogin
         onSuccess={credentialResponse => {
           console.log(credentialResponse);
@@ -15,9 +25,27 @@ function App() {
         onError={() => {
           console.log('Login Failed');
         }}
-      />;
+      />
+      <AuthenticatedTemplate>
+        {activeAccount ? (
+          <IdTokenData idTokenClaims={activeAccount.idTokenClaims} />
+        ) : null}
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <button className="signInButton" onClick={handleRedirect} >
+          Sign up
+        </button>
+      </UnauthenticatedTemplate>
     </>
   )
 }
+
+const App = ({ instance }) => {
+  return (
+    <MsalProvider instance={instance}>
+      <MainContent />
+    </MsalProvider>
+  );
+};
 
 export default App
